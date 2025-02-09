b7281f5da83da3235ec88348a409aefe17c15ac9
G. Endignoux <ggendx@gmail.com>
2025-02-09 21:19:20 +0100

    Benchmark several compression formats.
    ```
    Parsed 1137178883 bytes from 30466 files (+ 21 failed files)
    Expanded to 1322690141 bytes in memory (relative size = 116.31%)
    Optimized to 7264110 bytes (relative size = 0.64%)
    [89.93%] Interners: 6532926 bytes
    - [25.03%] String interner: 8050 objects | 1818466 bytes (225.90 bytes/object) | 17309489 references (2150.25 refs/object)
    - [2.25%] Uuid interner: 6617 objects | 163296 bytes (24.68 bytes/object) | 4735218 references (715.61 refs/object)
    - [10.18%] InternedSet<Disruption> interner: 7066 objects | 739652 bytes (104.68 bytes/object) | 30430 references (4.31 refs/object)
      - [9.90%] Disruption interner: 7550 objects | 719148 bytes (95.25 bytes/object) | 625760 references (82.88 refs/object)
        - [2.09%] ApplicationPeriod interner: 5883 objects | 151552 bytes (25.76 bytes/object) | 631593 references (107.36 refs/object)
    - [15.32%] InternedSet<Line> interner: 7183 objects | 1112896 bytes (154.93 bytes/object) | 30430 references (4.24 refs/object)
      - [7.74%] Line interner: 6880 objects | 562488 bytes (81.76 bytes/object) | 930026 references (135.18 refs/object)
        - [0.02%] LineHeader interner: 45 objects | 1428 bytes (31.73 bytes/object) | 930026 references (20667.24 refs/object)
        - [12.41%] ImpactedObject interner: 55373 objects | 901816 bytes (16.29 bytes/object) | 3183332 references (57.49 refs/object)
          - [0.48%] Object interner: 1679 objects | 34564 bytes (20.59 bytes/object) | 3183332 references (1895.97 refs/object)
          - [4.51%] InternedSet<Uuid> interner: 9127 objects | 327620 bytes (35.90 bytes/object) | 3183332 references (348.78 refs/object)
    +---------------+-------------------+-------------------+-------------------+-------------------+
    |    Format     |       Bytes       |      gzip -6      |       xz -6       |     brotli -6     |
    +---------------+-----------+-------+-----------+-------+-----------+-------+-----------+-------+
    | Bincode       |   5437330 | 0.48% |    893271 | 0.08% |    529996 | 0.05% |    700194 | 0.06% |
    | CBOR          |  17484567 | 1.54% |   1187575 | 0.10% |    615124 | 0.05% |    826739 | 0.07% |
    | JSON          |  26485131 | 2.33% |   1253219 | 0.11% |    736036 | 0.06% |    916683 | 0.08% |
    | JSON (pretty) |  65026281 | 5.72% |   1563418 | 0.14% |    958624 | 0.08% |   1006035 | 0.09% |
    | Postcard      |   3275869 | 0.29% |    861917 | 0.08% |    539200 | 0.05% |    721120 | 0.06% |
    +---------------+---------+-+-------+---------+-+-------+---------+-+-------+---------+-+-------+
    |               |   enc   |   dec   |   enc   |   dec   |   enc   |   dec   |   enc   |   dec   |
    +---------------+---------+---------+---------+---------+---------+---------+---------+---------+
    | Bincode       |    9 ms |   24 ms |  166 ms |   28 ms | 1111 ms |   39 ms |  191 ms |   19 ms |
    | CBOR          |   72 ms |  131 ms |  189 ms |   63 ms | 3255 ms |   51 ms |  230 ms |   36 ms |
    | JSON          |   72 ms |  100 ms |  233 ms |   96 ms | 3314 ms |   60 ms |  449 ms |   44 ms |
    | JSON (pretty) |  137 ms |  129 ms |  376 ms |  180 ms | 2934 ms |   81 ms |  441 ms |   74 ms |
    | Postcard      |   22 ms |   27 ms |   88 ms |   21 ms |  583 ms |   37 ms |  144 ms |   18 ms |
    +---------------+---------+---------+---------+---------+---------+---------+---------+---------+
    ```

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-#![feature(iter_order_by)]
+#![feature(exit_status_error, iter_order_by)]
 
 mod intern;
 mod schema;
@@ -12,6 +12,8 @@ use std::fmt::Debug;
 use std::fs::{read_dir, DirEntry, File};
 use std::io::{Read, Write};
 use std::path::{Path, PathBuf};
+use std::process::{Child, Command, Stdio};
+use std::thread;
 use std::time::{Duration, Instant};
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
@@ -127,23 +129,23 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         |bytes| Ok(postcard::from_bytes(bytes)?),
     )?;
 
-    println!("+---------------+-------------------+");
-    println!("|    Format     |       Bytes       |");
-    println!("+---------------+-----------+-------+");
+    println!("+---------------+-------------------+-------------------+-------------------+-------------------+");
+    println!("|    Format     |       Bytes       |      gzip -6      |       xz -6       |     brotli -6     |");
+    println!("+---------------+-----------+-------+-----------+-------+-----------+-------+-----------+-------+");
     bincode_bytes.print_sizes("Bincode", total_input_bytes);
     cbor_bytes.print_sizes("CBOR", total_input_bytes);
     json_bytes.print_sizes("JSON", total_input_bytes);
     json_pretty_bytes.print_sizes("JSON (pretty)", total_input_bytes);
     postcard_bytes.print_sizes("Postcard", total_input_bytes);
-    println!("+---------------+---------+-+-------+");
-    println!("|               |   enc   |   dec   |");
-    println!("+---------------+---------+---------+");
+    println!("+---------------+---------+-+-------+---------+-+-------+---------+-+-------+---------+-+-------+");
+    println!("|               |   enc   |   dec   |   enc   |   dec   |   enc   |   dec   |   enc   |   dec   |");
+    println!("+---------------+---------+---------+---------+---------+---------+---------+---------+---------+");
     bincode_bytes.print_times("Bincode");
     cbor_bytes.print_times("CBOR");
     json_bytes.print_times("JSON");
     json_pretty_bytes.print_times("JSON (pretty)");
     postcard_bytes.print_times("Postcard");
-    println!("+---------------+---------+---------+");
+    println!("+---------------+---------+---------+---------+---------+---------+---------+---------+---------+");
 
     Ok(())
 }
@@ -187,26 +189,45 @@ struct Database {
     datas: Vec<schema::optimized::Data>,
 }
 
+struct Stats {
+    serialized: CodecStats,
+    gzip: CodecStats,
+    xz: CodecStats,
+    brotli: CodecStats,
+}
+
 struct CodecStats {
     encoded_size: usize,
     encode_time: Duration,
     decode_time: Duration,
 }
 
-impl CodecStats {
+impl Stats {
     fn print_sizes(&self, title: &str, total_bytes: usize) {
         println!(
-            "| {title:<13} | {:>9} | {:.02}% |",
-            self.encoded_size,
-            self.encoded_size as f64 * 100.0 / total_bytes as f64,
+            "| {title:<13} | {:>9} | {:.02}% | {:>9} | {:.02}% | {:>9} | {:.02}% | {:>9} | {:.02}% |",
+            self.serialized.encoded_size,
+            self.serialized.encoded_size as f64 * 100.0 / total_bytes as f64,
+            self.gzip.encoded_size,
+            self.gzip.encoded_size as f64 * 100.0 / total_bytes as f64,
+            self.xz.encoded_size,
+            self.xz.encoded_size as f64 * 100.0 / total_bytes as f64,
+            self.brotli.encoded_size,
+            self.brotli.encoded_size as f64 * 100.0 / total_bytes as f64,
         );
     }
 
     fn print_times(&self, title: &str) {
         println!(
-            "| {title:<13} |{:>5} ms |{:>5} ms |",
-            self.encode_time.as_millis(),
-            self.decode_time.as_millis(),
+            "| {title:<13} |{:>5} ms |{:>5} ms |{:>5} ms |{:>5} ms |{:>5} ms |{:>5} ms |{:>5} ms |{:>5} ms |",
+            self.serialized.encode_time.as_millis(),
+            self.serialized.decode_time.as_millis(),
+            self.gzip.encode_time.as_millis(),
+            self.gzip.decode_time.as_millis(),
+            self.xz.encode_time.as_millis(),
+            self.xz.decode_time.as_millis(),
+            self.brotli.encode_time.as_millis(),
+            self.brotli.decode_time.as_millis(),
         );
     }
 }
@@ -216,7 +237,7 @@ fn serde_round_trip<T: PartialEq + Debug>(
     path: impl AsRef<Path> + Debug,
     serialize: impl FnOnce(&T) -> Result<Vec<u8>, Box<dyn std::error::Error>>,
     deserialize: impl FnOnce(&[u8]) -> Result<T, Box<dyn std::error::Error>>,
-) -> Result<CodecStats, Box<dyn std::error::Error>> {
+) -> Result<Stats, Box<dyn std::error::Error>> {
     eprintln!("- Serializing to: {path:?}");
 
     eprint!("Serializing...");
@@ -245,9 +266,141 @@ fn serde_round_trip<T: PartialEq + Debug>(
     f.write_all(&serialized)?;
     drop(f);
 
+    Ok(Stats {
+        serialized: CodecStats {
+            encoded_size: serialized.len(),
+            encode_time,
+            decode_time,
+        },
+        gzip: gzip_round_trip(&serialized)?,
+        xz: xz_round_trip(&serialized)?,
+        brotli: brotli_round_trip(&serialized)?,
+    })
+}
+
+fn gzip_round_trip(bytes: &[u8]) -> Result<CodecStats, Box<dyn std::error::Error>> {
+    codec_round_trip(
+        "gzip",
+        bytes,
+        || {
+            let mut command = Command::new("gzip");
+            command.arg("-c").arg("-6");
+            command
+        },
+        || {
+            let mut command = Command::new("gzip");
+            command.arg("-c").arg("-d");
+            command
+        },
+    )
+}
+
+fn xz_round_trip(bytes: &[u8]) -> Result<CodecStats, Box<dyn std::error::Error>> {
+    codec_round_trip(
+        "xz",
+        bytes,
+        || {
+            let mut command = Command::new("xz");
+            command.arg("-c").arg("-6");
+            command
+        },
+        || {
+            let mut command = Command::new("xz");
+            command.arg("-c").arg("-d");
+            command
+        },
+    )
+}
+
+fn brotli_round_trip(bytes: &[u8]) -> Result<CodecStats, Box<dyn std::error::Error>> {
+    codec_round_trip(
+        "brotli",
+        bytes,
+        || {
+            let mut command = Command::new("brotli");
+            command.arg("-c").arg("-6");
+            command
+        },
+        || {
+            let mut command = Command::new("brotli");
+            command.arg("-c").arg("-d");
+            command
+        },
+    )
+}
+
+fn codec_round_trip(
+    title: &str,
+    bytes: &[u8],
+    compress: impl FnOnce() -> Command,
+    decompress: impl FnOnce() -> Command,
+) -> Result<CodecStats, Box<dyn std::error::Error>> {
+    eprint!("[{title}] Compressing {} bytes...", bytes.len());
+    let start = Instant::now();
+    let compressed: Vec<u8> = io_command(
+        compress()
+            .stdin(Stdio::piped())
+            .stdout(Stdio::piped())
+            .stderr(Stdio::null())
+            .spawn()?,
+        bytes,
+    )?;
+    let encode_time = Instant::now().duration_since(start);
+    eprintln!(
+        " {:?} | {:.02} MB/s",
+        encode_time,
+        compressed.len() as f64 / (1_000_000.0 * encode_time.as_secs_f64()),
+    );
+
+    // Decompress to validate that compression worked properly.
+    eprint!("[{title}] Decompressing {} bytes...", compressed.len());
+    let start = Instant::now();
+    let decompressed: Vec<u8> = io_command(
+        decompress()
+            .stdin(Stdio::piped())
+            .stdout(Stdio::piped())
+            .stderr(Stdio::null())
+            .spawn()?,
+        &compressed,
+    )?;
+    let decode_time = Instant::now().duration_since(start);
+    eprintln!(
+        " {:?} | {:.02} MB/s",
+        decode_time,
+        compressed.len() as f64 / (1_000_000.0 * decode_time.as_secs_f64()),
+    );
+
+    assert_eq!(decompressed, bytes);
     Ok(CodecStats {
-        encoded_size: serialized.len(),
+        encoded_size: compressed.len(),
         encode_time,
         decode_time,
+    })
+}
+
+fn io_command(mut child: Child, input: &[u8]) -> Result<Vec<u8>, Box<dyn std::error::Error>> {
+    thread::scope(|s| -> Result<Vec<u8>, Box<dyn std::error::Error>> {
+        let mut stdin = child.stdin.take().expect("Failed to open stdin");
+        let mut stdout = child.stdout.take().expect("Failed to open stdout");
+
+        let input_thread = s.spawn(move || -> std::io::Result<()> {
+            stdin.write_all(input)?;
+            drop(stdin);
+            Ok(())
+        });
+
+        let output_thread = s.spawn(move || -> std::io::Result<Vec<u8>> {
+            let mut output = Vec::new();
+            stdout.read_to_end(&mut output)?;
+            Ok(output)
+        });
+
+        child.wait()?.exit_ok()?;
+
+        input_thread.join().expect("Failed to join input thread")?;
+        let output = output_thread
+            .join()
+            .expect("Failed to join output thread")?;
+        Ok(output)
     })
 }
