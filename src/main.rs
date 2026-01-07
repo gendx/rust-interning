963ee21b7aff6adb8234082fd51ded3a3b598ad0
G. Endignoux <ggendx@gmail.com>
2026-01-07 10:06:36 +0100

    Use Rayon to process input files in parallel.
    ```
    Parsed 8338856470 bytes from 215663 files (+ 1403 failed files)
    Expanded to 9735221738 bytes in memory (relative size = 116.75%)
    Optimized to 59699693 bytes (relative size = 0.72%)
    [91.33%] Interners: 54523781 bytes
    - [21.94%] String interner: 45347 objects | 13097397 bytes (288.83 bytes/object) | 121045183 references (2669.31 refs/object)
    - [2.47%] Uuid interner: 73606 objects | 1472688 bytes (20.01 bytes/object) | 34008137 references (462.03 refs/object)
    - [14.14%] InternedSet<Disruption> interner: 71850 objects | 8440144 bytes (117.47 bytes/object) | 215621 references (3.00 refs/object)
      - [12.45%] Disruption interner: 80593 objects | 7435036 bytes (92.25 bytes/object) | 5095927 references (63.23 refs/object)
        - [2.00%] ApplicationPeriod interner: 59600 objects | 1192568 bytes (20.01 bytes/object) | 5156967 references (86.53 refs/object)
    - [15.97%] InternedSet<Line> interner: 70584 objects | 9534752 bytes (135.08 bytes/object) | 215621 references (3.05 refs/object)
      - [8.37%] Line interner: 69506 objects | 4995856 bytes (71.88 bytes/object) | 6183623 references (88.97 refs/object)
        - [0.00%] LineHeader interner: 50 objects | 1768 bytes (35.36 bytes/object) | 6183623 references (123672.46 refs/object)
        - [9.89%] ImpactedObject interner: 491878 objects | 5903104 bytes (12.00 bytes/object) | 21574790 references (43.86 refs/object)
          - [0.05%] Object interner: 1860 objects | 30328 bytes (16.31 bytes/object) | 21574790 references (11599.35 refs/object)
          - [4.05%] InternedSet<Uuid> interner: 88824 objects | 2420140 bytes (27.25 bytes/object) | 21574790 references (242.89 refs/object)
    +---------------+-------------------+-------------------+-------------------+-------------------+
    |    Format     |       Bytes       |      gzip -6      |       xz -6       |     brotli -6     |
    +---------------+-----------+-------+-----------+-------+-----------+-------+-----------+-------+
    | Bincode       |  46369377 | 0.56% |   8084935 | 0.10% |   5053072 | 0.06% |   6386563 | 0.08% |
    | CBOR          |  32760760 | 0.39% |   7801108 | 0.09% |   5111456 | 0.06% |   6415698 | 0.08% |
    | JSON          |  49418090 | 0.59% |   8214046 | 0.10% |   5175816 | 0.06% |   7267712 | 0.09% |
    | JSON (pretty) | 130576806 | 1.57% |   9606283 | 0.12% |   5997828 | 0.07% |   8570088 | 0.10% |
    | Postcard      |  27438922 | 0.33% |   7360805 | 0.09% |   5131464 | 0.06% |   6219244 | 0.07% |
    +---------------+---------+-+-------+---------+-+-------+---------+-+-------+---------+-+-------+
    |               |   enc   |   dec   |   enc   |   dec   |   enc   |   dec   |   enc   |   dec   |
    +---------------+---------+---------+---------+---------+---------+---------+---------+---------+
    | Bincode       |  274 ms |  245 ms | 1412 ms |  210 ms | 7940 ms |  173 ms | 1560 ms |  123 ms |
    | CBOR          |  258 ms |  380 ms | 1075 ms |  170 ms | 6215 ms |  186 ms | 1610 ms |  109 ms |
    | JSON          |  251 ms |  493 ms | 1713 ms |  220 ms |15827 ms |  210 ms | 2117 ms |  141 ms |
    | JSON (pretty) |  416 ms |  548 ms | 1440 ms |  510 ms |17149 ms |  178 ms | 2250 ms |  212 ms |
    | Postcard      |  206 ms |  191 ms |  752 ms |  161 ms | 6067 ms |  215 ms | 1299 ms |  109 ms |
    +---------------+---------+---------+---------+---------+---------+---------+---------+---------+
    ```

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ mod schema;
 mod size;
 
 use intern::EqWith;
+use rayon::prelude::*;
 use schema::optimized::Interners;
 use serde::{Deserialize, Serialize};
 use size::EstimateSize;
@@ -13,15 +14,17 @@ use std::fs::{read_dir, DirEntry, File};
 use std::io::{Read, Write};
 use std::path::{Path, PathBuf};
 use std::process::{Child, Command, Stdio};
+use std::sync::atomic::{AtomicUsize, Ordering};
+use std::sync::Mutex;
 use std::thread;
 use std::time::{Duration, Instant};
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let mut file_count = 0;
-    let mut file_error_count = 0;
-    let mut total_input_bytes = 0;
-    let mut total_parsed_bytes = 0;
-    let mut total_optimized_bytes = 0;
+    let file_count = AtomicUsize::new(0);
+    let file_error_count = AtomicUsize::new(0);
+    let total_input_bytes = AtomicUsize::new(0);
+    let total_parsed_bytes = AtomicUsize::new(0);
+    let total_optimized_bytes = AtomicUsize::new(0);
 
     let mut args = std::env::args();
     if args.len() <= 2 {
@@ -31,43 +34,50 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     }
 
     let interners = Interners::default();
-    let mut datas = Vec::new();
+    let datas = Mutex::new(Vec::new());
 
     args.next(); // Ignoring the program path.
     let output_dir: PathBuf = args.next().unwrap().into();
     for directory in args {
         eprintln!("Visiting directory: {directory:?}");
-        visit_dirs(&directory, &mut |file_path| {
+        visit_dirs(&directory, &|file_path| {
             let mut file = File::open(file_path)?;
             let mut data = Vec::new();
             file.read_to_end(&mut data)?;
-            total_input_bytes += data.len();
+            total_input_bytes.fetch_add(data.len(), Ordering::Relaxed);
 
             let data: Result<schema::source::Data, _> = serde_json::from_slice(&data);
             let data = match data {
                 Ok(data) => data,
                 Err(err) => {
                     eprintln!("Error parsing JSON in file: {file_path:?}\n\t{err:?}");
-                    file_error_count += 1;
+                    file_error_count.fetch_add(1, Ordering::Relaxed);
                     return Ok(());
                 }
             };
-            total_parsed_bytes += data.estimated_bytes();
+            total_parsed_bytes.fetch_add(data.estimated_bytes(), Ordering::Relaxed);
 
             let optimized = schema::optimized::Data::from(&interners, data.clone());
-            total_optimized_bytes += optimized.estimated_bytes();
+            total_optimized_bytes.fetch_add(optimized.estimated_bytes(), Ordering::Relaxed);
 
             assert!(
                 optimized.eq_with(&data, &interners),
                 "Optimized data didn't match original for file: {file_path:?}"
             );
 
-            datas.push(optimized);
+            datas.lock().unwrap().push(optimized);
 
-            file_count += 1;
+            file_count.fetch_add(1, Ordering::Relaxed);
             Ok(())
         })?;
     }
+
+    let file_count = file_count.load(Ordering::Relaxed);
+    let file_error_count = file_error_count.load(Ordering::Relaxed);
+    let total_input_bytes = total_input_bytes.load(Ordering::Relaxed);
+    let total_parsed_bytes = total_parsed_bytes.load(Ordering::Relaxed);
+    let mut total_optimized_bytes = total_optimized_bytes.load(Ordering::Relaxed);
+    let datas = datas.into_inner().unwrap();
 
     println!("Parsed {total_input_bytes} bytes from {file_count} files (+ {file_error_count} failed files)");
     println!(
@@ -152,33 +162,37 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 
 fn visit_dirs(
     dir: impl AsRef<Path> + Debug,
-    callback: &mut impl FnMut(&Path) -> std::io::Result<()>,
+    callback: &(impl Fn(&Path) -> std::io::Result<()> + Sync),
 ) -> std::io::Result<()> {
     eprintln!("Reading directory: {dir:?}");
 
     // Sort entries by path for reproducibility.
     let mut entries: Vec<DirEntry> = read_dir(dir)?.collect::<Result<_, _>>()?;
     entries.sort_unstable_by_key(|x| x.path());
-    for entry in entries {
-        let mut path = entry.path();
-        let mut file_type = entry.file_type()?;
+    entries
+        .par_iter()
+        .try_for_each(|entry| -> std::io::Result<()> {
+            let mut path = entry.path();
+            let mut file_type = entry.file_type()?;
 
-        // Resolve symbolic links.
-        if file_type.is_symlink() {
-            eprint!("Resolving symlink: {path:?}");
-            path = std::fs::canonicalize(path)?;
-            eprintln!(" -> {path:?}");
-            file_type = path.metadata()?.file_type();
-        }
+            // Resolve symbolic links.
+            if file_type.is_symlink() {
+                eprint!("Resolving symlink: {path:?}");
+                path = std::fs::canonicalize(path)?;
+                eprintln!(" -> {path:?}");
+                file_type = path.metadata()?.file_type();
+            }
 
-        if file_type.is_dir() {
-            visit_dirs(path, callback)?;
-        } else if file_type.is_file() {
-            callback(&path)?;
-        } else {
-            eprintln!("Skipping path of unknown file type {file_type:?}: {path:?}");
-        }
-    }
+            if file_type.is_dir() {
+                visit_dirs(path, callback)?;
+            } else if file_type.is_file() {
+                callback(&path)?;
+            } else {
+                eprintln!("Skipping path of unknown file type {file_type:?}: {path:?}");
+            }
+
+            Ok(())
+        })?;
 
     Ok(())
 }
